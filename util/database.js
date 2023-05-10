a09fad717649ca46f2016e347a15a28478b89ba5
Jashanpreet20 <jashankot20@gmail.com>
2023-05-10 22:37:34 +0530

    util folder

---
+++ util/database.js
@@ -0,0 +1,8 @@
+const Sequelize=require('sequelize');
+
+const sequelize= new Sequelize(`${process.env.SQL_DATABASE}`,`${process.env.SQL_USER}`,`${process.env.SQL_PASSWORD}`,{
+    dialect: 'mysql',
+    host: process.env.HOSTNAME
+});
+
+module.exports=sequelize;
